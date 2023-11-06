bba3314e67a6de35edb64d5983171dda92bb442f
Drospi <drospigliozi@gmail.com>
2023-11-06 10:33:56 -0400

    v1.0.1

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<html>
+<!DOCTYPE html>
+<html lang="en" class="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1>Login</h1>
+    
+    <input type="text" name="email" id="email">
+    <input type="text" name="password" id="password">
+    <button>Login</button>
+
+    <p class="">or continue with these social profile</p>
+    <div  class="bg-white dark:bg-black">
+        <a href="">1</a>
+        <a href="">2</a>
+        <a href="">3</a>
+        <a href="">4</a>
+    </div>
+    <p>Ya eres un miembro? </p><a href="">Login</a>
+<?php
+?>
+    
+</body>
+</html>
+</html>
